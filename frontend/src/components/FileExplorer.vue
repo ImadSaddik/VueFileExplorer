b76bfd9e07dd973c01356050beb56c4c655a7a2a
Imad Saddik <simad3647@gmail.com>
2025-01-11 11:44:27 +0100

    Loading the JSON file in the FileExplorer component

---
+++ frontend/src/components/FileExplorer.vue
@@ -0,0 +1,15 @@
+<template>Hi world</template>
+
+<script>
+import fileTreeData from "@/assets/file_tree.json";
+
+export default {
+  name: "FileExplorer",
+  components: {},
+  data() {
+    return {
+      fileTree: fileTreeData,
+    };
+  },
+};
+</script>
