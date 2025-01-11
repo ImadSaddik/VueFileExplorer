89a82e5b5d72dff3fdf03633125dc7c64fd39332
Imad Saddik <simad3647@gmail.com>
2025-01-11 11:34:13 +0100

    Added a script to generate the file tree

---
+++ backend/main.py
@@ -0,0 +1,32 @@
+import os
+import json
+
+
+def get_file_tree(dir_path: str, items_to_ignore: list[str]) -> list[dict]:
+    file_tree = []
+    for item in os.listdir(dir_path):
+        if item in items_to_ignore:
+            continue
+
+        full_path = os.path.join(dir_path, item)
+        if os.path.isdir(full_path):
+            file_tree.append({
+                "name": item,
+                "type": "folder",
+                "children": get_file_tree(full_path, items_to_ignore)
+            })
+        else:
+            file_tree.append({
+                "name": item,
+                "type": "file"
+            })
+    return file_tree
+
+
+if __name__ == "__main__":
+    items_to_ignore = ['node_modules']
+    file_tree = get_file_tree(
+        dir_path=os.path.dirname(os.getcwd()),
+        items_to_ignore=items_to_ignore
+    )
+    json.dump(file_tree, open("file_tree.json", "w"), indent=2)
