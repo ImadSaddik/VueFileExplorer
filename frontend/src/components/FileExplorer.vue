d19ae4fd57f6500706d4b4424bfd528ec5c3eb7e
Imad Saddik <simad3647@gmail.com>
2025-01-11 11:46:55 +0100

    Used the FileNode in the FileExplorer

--- frontend/src/components/FileExplorer.vue
+++ frontend/src/components/FileExplorer.vue
@@ -1,11 +1,20 @@
-<template>Hi world</template>
+<template>
+  <div class="file-explorer">
+    <ul>
+      <FileNode v-for="(node, index) in fileTree" :key="index" :node="node" />
+    </ul>
+  </div>
+</template>
 
 <script>
 import fileTreeData from "@/assets/file_tree.json";
+import FileNode from "@/components/FileNode.vue";
 
 export default {
   name: "FileExplorer",
-  components: {},
+  components: {
+    FileNode,
+  },
   data() {
     return {
       fileTree: fileTreeData,
